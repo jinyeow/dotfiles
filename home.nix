bf684e0470c2924d7f535f0bb7f058985e9ca904
Justin Puah <justin@puah.dev>
2022-12-28 06:12:55 +0000

    update README.md with further installation instructions. Update home.nix

--- home.nix
+++ home.nix
@@ -21,6 +21,7 @@
   
   # Raw configuration files
   home.file.".vimrc".source = ./vimrc;
+  home.file.".gitconfig".source = ./gitconfig;
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
