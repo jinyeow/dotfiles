ae6e2a698dbaacc88812b27bce50f89ffd9d06de
jinyeow <justin@puah.dev>
2022-12-28 07:50:41 +0000

    remove fish package from home.nix. set bash as default shell. add source nix file in profile

--- home.nix
+++ home.nix
@@ -5,23 +5,72 @@
   # paths it should manage.
   home.username = "jinyeow";
   home.homeDirectory = "/home/jinyeow";
+  
+  # Environment Variables
+  home.sessionVariables = {
+    EDITOR = "vim";
+  };
 
   # Packages to install
   home.packages = [
     # pkgs is the set of all packages in the default home.nix implementation
     pkgs.bash
-    pkgs.fish
+    pkgs.exa
     pkgs.fzf
+    pkgs.go
+    pkgs.jq
     pkgs.neovim
     pkgs.openssh
-    pkgs.sshs
+    pkgs.readline
+    pkgs.ripgrep
+    pkgs.tig
     pkgs.tmux
     pkgs.vim
+    pkgs.yq
+    pkgs.zoxide
   ];
   
   # Raw configuration files
+  home.file.".bashrc".source = ./bashrc;
+  home.file.".bash_aliases".source = ./bash/bash_aliases;
+  home.file.".bash_profile".source = ./bash/bash_profile;
+  home.file.".inputrc".source = ./inputrc;
+  home.file.".profile".source = ./profile;
+  home.file.".tmux.conf".source = ./tmux.conf;
   home.file.".vimrc".source = ./vimrc;
-  home.file.".gitconfig".source = ./gitconfig;
+
+  # home.file.".gitconfig".source = ./gitconfig;
+
+  # Git config using Home Manager modules
+  programs.git = {
+    enable = true;
+    userName = "jinyeow";
+    userEmail = "justin@puah.dev";
+    aliases = {
+      a = "add";
+      aliases = "config --get-regexp alias";
+      amend = "commit --amend";
+      branches = "branch -a";
+      c = "commit";
+      co = "checkout";
+      ctags = "!.git/hooks/ctags";
+      delete = "rm -r --cached";
+      discard = "checkout --";
+      filediff = "diff --name-status";
+      graph = "log --graph -50 --branches --remotes --tags  --format=format:'%Cgreen%h %Creset• <(75,trunc)%s (%cN, %cr) %Cred%d' --date-order";
+      ignore = "'!gi() { curl -L -s https://www.gitignore.io/api/$@ ;}; gi'";
+      last = "log -1 HEAD --format=format:\"%Cred%H\"";
+      lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+      remotes = "remote -v";
+      s = "status";
+      stashes = "stash list";
+      tags = "tag";
+      uncommit = "reset --mixed HEAD~";
+      unstage = "reset -q HEAD --";
+      unmerged = "diff --name-only --diff-filter=U";
+      test = "status";
+    };
+  };
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
