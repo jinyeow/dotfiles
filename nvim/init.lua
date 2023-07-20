2560412082bcc7563662fe315e417fa4989cf3cb
Justin Puah <jpuah@nrl.com.au>
2023-07-20 15:08:09 +1000

    [NVIM-INIT]-Fix typo in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -131,7 +131,7 @@ require('lazy').setup({
   {
     -- Catppuccin theme (https://github.com/catppuccin/nvim)
     'catppuccin/nvim',
-    name = "catppuccin"
+    name = "catppuccin",
     priority = 1000,
     config = function()
       vim.cmd.colorscheme 'catppuccin' -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
@@ -191,3 +191,45 @@ require('lazy').setup({
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   -- { import = 'custom.plugins' },
 }, {})
+
+
+-- TODO
+-- require("catppuccin").setup({
+--   flavour = "mocha", -- latte, frappe, macchiato, mocha
+--   background = {
+--     light = "latte",
+--     dark = "mocha",
+--   },
+--   transparent_background = false, -- disables setting the background color.
+--   show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+--   term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+--   dim_inactive = {
+--       enabled = false, -- dims the background color of inactive window
+--       shade = "dark",
+--       percentage = 0.15, -- percentage of the shade to apply to the inactive window
+--   },
+--   no_italic = false, -- Force no italic
+--   no_bold = false, -- Force no bold
+--   no_underline = false, -- Force no underline
+--   styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+--       comments = { "italic" }, -- Change the style of comments
+--       conditionals = { "italic" },
+--       loops = {},
+--       functions = {},
+--       keywords = {},
+--       strings = {},
+--       variables = {},
+--       numbers = {},
+--       booleans = {},
+--       properties = {},
+--       types = {},
+--       operators = {},
+--   },
+--   color_overrides = {},
+--   custom_highlights = {},
+--   integrations = {
+--       gitsigns = true,
+--       -- For more plugins integrations (https://github.com/catppuccin/nvim#integrations)
+--   },
+-- })
+-- vim.cmd.colorscheme 'catppuccin' -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
