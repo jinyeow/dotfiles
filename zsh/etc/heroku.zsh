ffab5069c994c56a8ec39318ccab7a3b10e783b2
Justin Puah <justin@puah.dev>
2023-07-22 09:23:25 +1000

    Remove old and unused dotfiles to clean up dotfiles repo

--- zsh/etc/heroku.zsh
+++
@@ -1,8 +0,0 @@
-##########
-# HEROKU #
-##########
-
-# From 'github.com/salimane/dotfiles/blob/master/zsh/.zsh/etc/heroku.zsh'
-
-### Added by the Heroku Toolbelt
-export PATH="/usr/local/heroku/bin:$PATH"
