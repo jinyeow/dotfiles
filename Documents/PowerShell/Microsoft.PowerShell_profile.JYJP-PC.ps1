db5f2adef4a7880e4f02f66f6dcb49915e5d248f
Justin Puah <justin@puah.dev>
2025-12-04 09:19:48 +1100

    Update JYJP-PC PowerShell PROFILE

--- Documents/PowerShell/Microsoft.PowerShell_profile.JYJP-PC.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.JYJP-PC.ps1
@@ -1,5 +1,8 @@
 # gist:4f51b2f23ae8b90e160877e8a8f29bb5
 #Requires -Version 7
+#Requires -Module @{ ModuleName = 'PSReadLine'; ModuleVersion = '2.2.0' }
+
+using module PSReadLine
 
 Import-Module posh-git
 
@@ -14,61 +17,88 @@ Import-Module posh-git
 		iex ((New-Object System.Net.WebClient).DownloadString(‘https://community.chocolatey.org/install.ps1'))
 #>
 
-function OnViModeChange {
-    if ($args[0] -eq 'Command') {
-        # Set the cursor to a blinking block
-        Write-Host -NoNewline "`e[1 q"
-    } else {
-        # Set the cursor to a blinking line
-        Write-Host -NoNewline "`e[5 q"
+# === PSReadLine ===
+if (Get-Module -ListAvailable -Name 'PsReadLine' -ErrorAction SilentlyContinue) {
+    if ((Get-Module PSReadLine).Version -lt 2.2) {
+        Write-Error 'Profile requires PSReadLine 2.2+' -ErrorAction Stop
     }
+
+    Set-PSReadLineOption -PredictionSource History
+    Set-PSReadLineOption -PredictionViewStyle ListView
+    Set-PSReadLineOption -ShowToolTips
+
+    Set-PSReadLineOption -Colors @{
+        Command = 'Yellow'
+        Parameter = 'Green'
+        String = 'DarkCyan'
+        InlinePrediction = 'DarkGray'
+    }
+
+    function OnViModeChange {
+        if ($args[0] -eq 'Command') {
+            # Set the cursor to a blinking block
+            Write-Host -NoNewline "`e[1 q"
+        } else {
+            # Set the cursor to a blinking line
+            Write-Host -NoNewline "`e[5 q"
+        }
+    }
+    $PSReadLineOptions = @{
+        EditMode                      = 'Vi'
+        HistoryNoDuplicates           = $true
+        HistorySearchCursorMovesToEnd = $true
+        HistorySaveStyle              = 'SaveIncrementally'
+        ViModeIndicator               = 'Script'
+        ViModeChangeHandler           = $Function:OnViModeChange
+    }
+    Set-PSReadLineOption @PSReadLineOptions
+
+    Set-PSReadLineKeyHandler -Key Ctrl+r -Function ReverseSearchHistory
+    Set-PSReadLineKeyHandler -Key Ctrl+Spacebar -Function MenuComplete
+    Set-PSReadLineKeyHandler -Key Tab -Function Complete
+
+    Set-PSReadLineKeyHandler -Chord Ctrl+Oem4 -Function ViCommandMode # NOTE: see https://github.com/PowerShell/PSReadLine/issues/906#issuecomment-916847040
+    Set-PSReadLineKeyHandler -Chord Shift+Tab -Function MenuComplete
+    Set-PSReadLineKeyHandler -Chord Ctrl+b -Function BackwardChar
+    Set-PSReadLineKeyHandler -Chord Ctrl+f -Function ForwardChar
+    Set-PSReadLineKeyHandler -Chord Ctrl+p -Function PreviousHistory
+    Set-PSReadLineKeyHandler -Chord Ctrl+n -Function NextHistory
+    Set-PSReadLineKeyHandler -Chord Ctrl+a -Function BeginningOfLine
+    Set-PSReadLineKeyHandler -Chord Ctrl+e -Function EndOfLine
+    Set-PSReadLineKeyHandler -Chord Ctrl+w -Function BackwardDeleteWord
+    Set-PSReadLineKeyHandler -Chord Ctrl+u -Function BackwardDeleteLine
 }
-
-# if ((Get-Module PSReadLine).Version -lt 2.2) {
-# 	throw 'Profile requires PSReadLine 2.2+'
-# }
-
-Import-Module PSReadLine
-
-$PSReadLineOptions = @{
-    EditMode                      = 'Vi'
-    HistoryNoDuplicates           = $true
-    HistorySearchCursorMovesToEnd = $true
-    HistorySaveStyle              = 'SaveIncrementally'
-    PredictionSource              = 'History'
-    PredictionViewStyle           = 'ListView'
-    ViModeIndicator               = 'Script'
-    ViModeChangeHandler           = $Function:OnViModeChange
-}
-
-Set-PSReadLineOption @PSReadLineOptions
-
-Set-PSReadLineKeyHandler -Chord Shift+Tab -Function MenuComplete
-Set-PSReadLineKeyHandler -Chord Ctrl+b -Function BackwardChar
-Set-PSReadLineKeyHandler -Chord Ctrl+f -Function ForwardChar
-Set-PSReadLineKeyHandler -Chord Ctrl+p -Function PreviousHistory
-Set-PSReadLineKeyHandler -Chord Ctrl+n -Function NextHistory
-Set-PSReadLineKeyHandler -Chord Ctrl+Oem4 -Function ViCommandMode # NOTE: see https://github.com/PowerShell/PSReadLine/issues/906#issuecomment-916847040
-Set-PSReadLineKeyHandler -Chord Ctrl+a -Function BeginningOfLine
-Set-PSReadLineKeyHandler -Chord Ctrl+e -Function EndOfLine
-Set-PSReadLineKeyHandler -Chord Ctrl+w -Function BackwardDeleteWord
 
 Import-Module Az.Tools.Predictor
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 
 # == PSFzf ==
-#Remove-PSReadLineKeyHandler 'Ctrl+r'
-#Remove-PSReadLineKeyHandler 'Ctrl+t'
-Import-Module PSFzf
+Import-Module PSFzf -Force -ErrorAction SilentlyContinue
 
-# replace standard tab completion
-Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
-Set-Alias -Name frg -Value Invoke-PsFzfRipgrep
-Set-PSReadLineKeyHandler -Chord 'alt+f' -ScriptBlock { Invoke-PsFzfRipgrep -SearchString '.' }
+if (Get-Module -ListAvailable -Name 'PsFzf' -ErrorAction SilentlyContinue) {
+    Set-Alias -Name frg -Value Invoke-PsFzfRipgrep
 
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+    # Replace standard TAB completion
+    Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+    Set-PSReadLineKeyHandler -Chord 'alt+f' -ScriptBlock { Invoke-PsFzfRipgrep -SearchString '.' }
 
-# NOTE: taken from - https://gist.github.com/SteveL-MSFT/a208d2bd924691bae7ec7904cab0bd8e
+    # example command - use $Location with a different command:
+    $commandOverride = [ScriptBlock] { param($Location) Write-Host $Location }
+
+    # pass your override to PSFzf:
+    $psFzfOverrides = @{
+        AltCCommand = $commandOverride
+        EnableAliasFuzzyEdit = $true
+        EnableAliasFuzzyGitStatus = $true
+        EnableAliasFuzzyHistory = $true
+        EnableAliasFuzzyKillProcess = $true
+        EnableAliasFuzzySetLocation = $true
+        PSReadlineChordProvider = 'Ctrl+t'
+        PSReadlineChordReverseHistory = 'Ctrl+r'
+    }
+    Set-PsFzfOption @psFzfOverrides
+}
+
 # == Prompt ==
 function parse_git_dirty {
     $STATUS = "$(git status)"
@@ -197,8 +227,12 @@ function prompt {
 
     $currentDirectory = dirtrim
     $userName = "$($color.Blue)$([string]::IsNullOrEmpty($Env:UserName) ? $Env:USER : $Env:UserName)$($color.Reset)"
+    $out = ''
+    if ($loc.Provider.Name -eq 'FileSystem') {
+        $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+    }
 
-    "${lastCmdTime}${userName} in ${currentDirectory}${gitBranch}${devBuild}`n${lastExit}PS$($color.Reset)$('>' * ($nestedPromptLevel + 1)) "
+    "${out}${lastCmdTime}${userName} in ${currentDirectory}${gitBranch}${devBuild}`n${lastExit}PS$($color.Reset)$('>' * ($nestedPromptLevel + 1)) "
 
     # set window title
     # try {
@@ -222,6 +256,27 @@ function prompt {
     $global:LASTEXITCODE = $currentLastExitCode
 }
 
+# === azcli Tab Completion ===
+if ($null -ne (Get-Command -Name az -ErrorAction SilentlyContinue)) {
+    Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
+        param($commandName, $wordToComplete, $cursorPosition)
+        $completion_file = New-TemporaryFile
+        $env:ARGCOMPLETE_USE_TEMPFILES = 1
+        $env:_ARGCOMPLETE_STDOUT_FILENAME = $completion_file
+        $env:COMP_LINE = $wordToComplete
+        $env:COMP_POINT = $cursorPosition
+        $env:_ARGCOMPLETE = 1
+        $env:_ARGCOMPLETE_SUPPRESS_SPACE = 0
+        $env:_ARGCOMPLETE_IFS = "`n"
+        $env:_ARGCOMPLETE_SHELL = 'powershell'
+        az 2>&1 | Out-Null
+        Get-Content $completion_file | Sort-Object | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+        Remove-Item $completion_file, Env:\_ARGCOMPLETE_STDOUT_FILENAME, Env:\ARGCOMPLETE_USE_TEMPFILES, Env:\COMP_LINE, Env:\COMP_POINT, Env:\_ARGCOMPLETE, Env:\_ARGCOMPLETE_SUPPRESS_SPACE, Env:\_ARGCOMPLETE_IFS, Env:\_ARGCOMPLETE_SHELL
+    }
+}
+
 # == STARTUP ==
 #if (-Not (Get-Module -ListAvailable -Name Pscx)) {
 #  Install-Module Pscx -Scope CurrentUser -Force
@@ -236,6 +291,7 @@ if ($PSVersionTable.PSVersion.Major -eq 5) {
 }
 
 Set-Alias c clear -Force
+Set-Alias -Name g -Value git
 Set-Alias gcif Get-ChildItem -Force
 
 # == FUNCTIONS ==
@@ -251,6 +307,11 @@ function golangci-lint {
     docker run --rm -v ${pwd}:/app -v ~/.cache/golangci-lint/1.53.2:/root/.cache -w /app golangci/golangci-lint:v1.53.2 golangci-lint run -v
 }
 
+function which ($command) {
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
+        Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
 # == ENVIRONMENT VARIABLES ===
 $env:XDG_CONFIG_HOME = "$HOME/.config"
 
@@ -260,7 +321,11 @@ if (Test-Path($ChocolateyProfile)) {
     Import-Module "$ChocolateyProfile"
 }
 # == zoxide ==
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
+# https://github.com/ajeetdsouza/zoxide
+# As per documentation, set at the end of the PROFILE
+if ($null -ne (Get-Command -Name zoxide -ErrorAction SilentlyContinue)) {
+    Invoke-Expression (& { (zoxide init powershell | Out-String) })
+}
 
 #f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
 
